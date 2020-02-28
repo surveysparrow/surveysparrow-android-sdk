310e4be9c89913f86362f550d258576f2bcf3e7b
Ajay Sivan <ajaysivan@surveysparrow.com>
2020-02-28 16:20:15 +0530

    Example movie app

---
+++ examples/movie_review_kotlin/app/src/main/java/com/surveysparrow/sdk/example/movie_review_kotlin/RecommendationsActivity.kt
@@ -0,0 +1,12 @@
+package com.surveysparrow.sdk.example.movie_review_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecommendationsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recommendations)
+    }
+}

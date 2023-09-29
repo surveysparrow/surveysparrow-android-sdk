f10cbbdfc56133f512527e84d2132e597111b5cf
Sivanesh <sivanesh.selvan@surveysparrow.com>
2023-09-29 15:29:38 +0530

    Survey throttling (#11)
    * surve-validation
    
    * old function support
    
    * Update README.md
    
    * survey-throttling
    
    * added wrap content
    
    * updated version code

--- survey-sparrow-android-sdk/src/main/java/com/surveysparrow/ss_android_sdk/SsSurveyFragment.java
+++ survey-sparrow-android-sdk/src/main/java/com/surveysparrow/ss_android_sdk/SsSurveyFragment.java
@@ -1,6 +1,5 @@
 package com.surveysparrow.ss_android_sdk;
 
-
 import android.animation.ObjectAnimator;
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -24,6 +23,9 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.FrameLayout;
 import android.widget.ProgressBar;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.widget.TextView;
 
 import com.surveysparrow.ss_android_sdk.SsSurvey.CustomParam;
 import android.os.AsyncTask;
@@ -42,18 +44,29 @@ import org.json.JSONObject;
 public final class SsSurveyFragment extends Fragment {
     private SsSurvey survey;
     private String activity;
+    private int widgetContactId = 0;
     private ProgressBar progressBar;
     private ObjectAnimator progressBarAnimator;
     public static final String LOG_TAG = "SS_SAMPLE";
     public static final String SS_VALIDATION = "SS_VALIDATION";
-    public JSONObject jsonObject; 
+    public JSONObject jsonObject;
+    public JSONObject closeSurveyJSONObject;
     private OnSsResponseEventListener onSsResponseEventListener;
+    public Boolean surveyCompleted = false;
     private OnSsValidateSurveyEventListener validationListener;
+    private OnSsCloseSurveyEventListener closeSurveyListener;
 
     public void setValidateSurveyListener(OnSsValidateSurveyEventListener listener) {
         validationListener = listener;
     }
 
+    public void setCloseSurveyListener(OnSsCloseSurveyEventListener listener) {
+        closeSurveyListener = listener;
+    }
+
+    public void setData(int id) {
+        widgetContactId = id;
+    }
     WebView ssWebView;
     /**
      * Create SsSurveyFragment public constructor.
@@ -89,41 +102,44 @@ public final class SsSurveyFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+        Bundle savedInstanceState) {
         // we are checking the activity name here to avoid duplicate api call                 
         if (!activity.equals("SsSurveyActivity")) {
             CustomParam[] customparam = survey.getCustomParams();
-            String apiUrl = "https://"+ survey.getDomain()+"/sdk/validate-survey/"+survey.getSurveyToken();
-            APICallTask apiCallTask = new APICallTask(apiUrl,customparam, new APICallTask.ApiCallback() {
-            @Override
-            public void onResponse(String response) {
-                try {
-                    jsonObject = new JSONObject(response);
-                } catch (JSONException e) {
-                    e.printStackTrace();
+            String apiUrl = "https://" + survey.getDomain() + "/sdk/validate-survey/" + survey.getSurveyToken();
+            APICallTask apiCallTask = new APICallTask(apiUrl, customparam, new APICallTask.ApiCallback() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        jsonObject = new JSONObject(response);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
             });
             apiCallTask.execute(apiUrl);
             try {
-                apiCallTask.await();  
+                apiCallTask.await();
             } catch (InterruptedException e) {
                 Log.e(SS_VALIDATION, "Error in apiCallTask" + e);
             }
             try {
-                if(jsonObject.getBoolean("active") != true){
-                    if(validationListener != null){
+                if (jsonObject.getBoolean("active") != true) {
+                    if (validationListener != null) {
                         validationListener.onSsValidateSurvey(jsonObject);
                         return null;
                     }
-                    Log.v(SS_VALIDATION, "survey validation error json" + jsonObject.toString() );
+                    Log.v(SS_VALIDATION, "survey validation error json" + jsonObject.toString());
                 }
-                } catch (Exception e) {
+                if (jsonObject.has("widgetContactId")) {
+                    widgetContactId = jsonObject.getInt("widgetContactId");
+                }
+            } catch (Exception e) {
                 Log.e(SS_VALIDATION, "Error in  processing  apiCallTask json" + e);
-            }    
-        }                  
+            }
+        }
 
-        FrameLayout ssLayout = new FrameLayout(getActivity());
+        final FrameLayout ssLayout = new FrameLayout(getActivity());
 
         progressBar = new ProgressBar(getActivity(), null, android.R.attr.progressBarStyleHorizontal);
         progressBar.setMax(100);
@@ -134,6 +150,64 @@ public final class SsSurveyFragment extends Fragment {
         ssWebView.getSettings().setDomStorageEnabled(true);
         ssWebView.addJavascriptInterface(new JsObject(), "SsAndroidSdk");
 
+        final TextView closeButton = new TextView(getActivity());
+        closeButton.setText("X");
+        closeButton.setTextSize(18);
+        closeButton.setTextColor(Color.BLACK);
+        closeButton.setGravity(Gravity.CENTER);
+        closeButton.setBackgroundColor(Color.TRANSPARENT);
+        closeButton.setClickable(true);
+        closeButton.setPadding(10, 10, 10, 10);
+        closeButton.setVisibility(View.GONE);
+
+        FrameLayout.LayoutParams closeButtonParams = new FrameLayout.LayoutParams(
+            FrameLayout.LayoutParams.WRAP_CONTENT,
+            FrameLayout.LayoutParams.WRAP_CONTENT
+        );
+        closeButtonParams.setMargins(0, 20, 60, 0);
+        closeButtonParams.gravity = Gravity.TOP | Gravity.END;
+        closeButton.setLayoutParams(closeButtonParams);
+
+        // Set an OnClickListener for the close button
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String closeSurveyUrl = "https://" + survey.getDomain() + "/nps/widget/contact/" + widgetContactId;
+                CloseSurvey closeSurvey = new CloseSurvey(closeSurveyUrl, survey.getSurveyToken(), widgetContactId, surveyCompleted, new CloseSurvey.CloseSurveyCallback() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            closeSurveyJSONObject = new JSONObject(response);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                closeSurvey.execute(closeSurveyUrl);
+                try {
+                    closeSurvey.await();
+                } catch (InterruptedException e) {
+                    Log.e(SS_VALIDATION, "Error in closeSurvey" + e);
+                }
+
+                try {
+                    if (closeSurveyJSONObject.getBoolean("surveyClosed") == true) {
+                        if (closeSurveyListener != null) {
+                            closeSurveyListener.onSsCloseSurveyEvent();
+                        } else {
+                            getActivity().runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    ssLayout.setVisibility(View.GONE);
+                                }
+                            });
+                        }
+                    }
+                } catch (Exception e) {
+                    Log.e(SS_VALIDATION, "Error in  processing  close survey json" + e);
+                }
+            }
+        });
         ssWebView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -152,6 +226,7 @@ public final class SsSurveyFragment extends Fragment {
                 super.onProgressChanged(view, newProgress);
                 if (newProgress == 100) {
                     progressBar.setVisibility(View.GONE);
+                    closeButton.setVisibility(View.VISIBLE);
                     return;
                 }
                 progressBarAnimator = ObjectAnimator.ofInt(progressBar, "progress", progressBar.getProgress(), newProgress);
@@ -164,13 +239,14 @@ public final class SsSurveyFragment extends Fragment {
 
         ssLayout.addView(ssWebView);
         ssLayout.addView(progressBar);
+        ssLayout.addView(closeButton);
         return ssLayout;
     }
-
 
     private class JsObject {
         @JavascriptInterface
         public void shareData(String data) {
+            surveyCompleted = true;
             onSsResponseEventListener.onSsResponseEvent(SurveySparrow.toJSON(data));
         }
     }

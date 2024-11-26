6eddd85a06e3ee150aa876baf14a4857bb854dfe
Kalaiselvan K <kalaiselvan.k@surveysparrow.com>
2024-11-26 19:20:02 +0530

    Photo Capture

--- survey-sparrow-android-sdk/build.gradle.kts
+++ survey-sparrow-android-sdk/build.gradle.kts
@@ -24,6 +24,7 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -39,15 +40,22 @@ android {
     }
 }
 
-dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.2.0-rc01")
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
-    testImplementation("junit:junit:4.12")
-    androidTestImplementation("androidx.test.ext:junit:1.1.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.fragment:fragment-ktx:1.6.1")
+    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation("androidx.annotation:annotation:1.6.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(kotlin("script-runtime"))
 }
+
+
 
 publishing {
     publications {
